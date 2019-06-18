897a3eb123b3920726dc8f5f1378c37b61fd8785
Seth Lewis <lewiscatenary@gmail.com>
2019-06-18 10:27:01 -0500

    added mouseover/out to header logo & focus/blur events to header links

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
 // Your code goes here
-const h1Logo = document.querySelector('logo-heading');
+
+// changes background color of header logo
+const h1Logo = document.querySelector('.logo-heading');
+h1Logo.addEventListener('mouseover', (e) => {
+  e.target.style.backgroundColor = 'purple';
+});
+h1Logo.addEventListener('mouseout', (e) => {
+  e.target.style.backgroundColor = 'white';
+});
+
+// changes fontsize of focused header links
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((anchor) => {
+  anchor.addEventListener('focus', (e) => {
+    e.target.style.fontSize = '2.0rem';
+  })
+});
+navLinks.forEach((anchor) => {
+  anchor.addEventListener('blur', (e) => {
+    e.target.style.fontSize = '1.6rem';
+  })
+});
